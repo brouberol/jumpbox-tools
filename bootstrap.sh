9dd66344998cd898b4a7d3bfd8dcb06f980bb043
Balthazar Rouberol <balthazar.rouberol@datadoghq.com>
2017-06-26 08:58:43 +0200

    Add gitconfig

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,7 +8,7 @@ rm -rf .bash* .bin* .cache* .chef* .config* .gitconfig .lesshst .oh-my-zsh .prof
 rm -rf .jumpbox-tools
 
 # Clone ourselves
-git clone git@github.com:bonnefoa/jumpbox-tools ~/.jumpbox-tools
+git clone git@github.com:brouberol/jumpbox-tools ~/.jumpbox-tools
 cd ~/.jumpbox-tools
 
 ./installs.sh
